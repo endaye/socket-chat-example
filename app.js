09b9a85798aa35fb5562d953d9a9784fd33e7ff3
Yuancheng Zhang <old.far.hot@gmail.com>
2018-02-28 17:54:56 -0600

    update

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
+const app = require('express')();
+const http = require('http').Server(app);
 var io = require('socket.io')(http);
+var total_node = 0;
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  total_node++;
+  console.log(`user connected: ${socket.id}\ntotal nodes: ${total_node}`);
+  io.emit('')
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    total_node--;
+    console.log(`user disconnected: ${socket.id}\ntotal nodes: ${total_node}`);
   });
 });
 
